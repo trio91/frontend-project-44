2d895e25f500a01a2d4ae20b615ee61e3bc50975
Dima <ciba91@gmail.com>
2024-08-25 11:14:44 +0300

    added function for greetings user

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
+import getUserName from "../src/cli.js";
 console.log('Welcome to the Brain Games!');
+getUserName();
---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+const getUserName  = () => {
+    const name = readlineSync.question('May I have Your name?');
+    console.log('Hello, ' + name + '!')
+}
+export  default getUserName;
