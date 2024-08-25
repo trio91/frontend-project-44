7e7058e219e06cb36318ac7217221ae98d0eb073
Dima <ciba91@gmail.com>
2024-08-25 12:43:12 +0300

    new

---
+++ bin/brain-even.js
@@ -0,0 +1,6 @@
+const game = () => {
+    const number = Math.random();
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    console.log(number);
+}
+export default game;
