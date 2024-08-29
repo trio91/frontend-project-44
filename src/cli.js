90788880f22d775a9918192fe6cf267484ecfb0c
Dima <ciba91@gmail.com>
2024-08-29 20:47:47 +0300

    fixed brain-games

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const getUserName = () => {
+  console.log('Welcome to the Brain games!');
   const name = readlineSync.question('May I have Your name?');
   console.log(`Hello, ${name}!`);
 };
