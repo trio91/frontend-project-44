37711e5166f09d6e526fcfaf06a2e3e04d7c848e
Dima <ciba91@gmail.com>
2024-08-29 20:32:04 +0300

    completed prime-game

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -1,19 +1,23 @@
 import { start, getNumber } from "../index.js";
 
 function isPrime (number) {
+    let result = '';
     for (let i = 2; i < number; i += 1) {
-        if (number % i === 0) return false
+        if (number % i === 0)  
+            return false; 
     }
-    return number !== 1;
-}
+        return  number !== 1;
+};
 
-
-
-
-
-
+const resultOfIt = () => {
+    const number = getNumber();
+    const question = `Question: ${number}`;
+    const correct = isPrime(number) ? 'yes' : 'no';
+    return [question, correct];
+};
 
 const game = () => {
-
-}
+    const startMessage = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    start (startMessage, resultOfIt);
+};
 export default game;
