bd5e94934f6470040604b1fdc01c8c98d55ad180
Dima <ciba91@gmail.com>
2024-09-03 12:46:38 +0300

    fixed calc game

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -6,21 +6,21 @@ const max = 100;
 const description = 'What is the result of the expression?';
 const operators = ['+', '-', '*'];
 
-const calculation = (operation, value1, value2) => {
-  switch (operation) {
+const calculation = (operator, value1, value2) => {
+  switch (operator) {
     case '+': return value1 + value2;
     case '-': return value1 - value2;
     case '*': return value1 * value2;
-    default: throw new Error(`Sorry operation ${operation} is undefined`); 
+    default: throw new Error(`Sorry operation ${operator} is undefined`); 
   }
 };
 
 const generateRound = () => {
   const number1 = getRandomNumber(min, max);
   const number2 = getRandomNumber(min, max);
-  const operator = getRandomIndex(operators);
+  const operator = operators[getRandomIndex(operators)];
   const question = `${number1} ${operator} ${number2}`;
-  const correctAnswer = calculation(expression, number1, number2).toString();
+  const correctAnswer = calculation(operator, number1, number2).toString();
   return [question, correctAnswer];
 };
 
